7abe8fceb8e71c91fdc8ed20b00352db013968f8
andyvkim <andyvkim@gmail.com>
2017-10-17 03:35:29 -0400

    Add files via upload

---
+++ Libs/Controllers/PatronController.cs
@@ -0,0 +1,39 @@
+﻿using Libs.Models.Patron;
+using LibsData.Models;
+using LibsService;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Libs.Controllers
+{
+
+    public class PatronController : Controller
+    {
+        private LibsMethodsServices _services;
+        public PatronController(LibsMethodsServices services)
+        {
+            _services = services;
+        }
+        public IActionResult PatronIndex()
+        {
+            IEnumerable<Patron> patronModels = _services.GetAllPatrons();
+
+            var listingResult = patronModels
+                .Select(result => new PatronModel
+                {
+                    Id = result.id,
+                    FirstName = result.FirstName,
+                    LastName = result.LastName
+                });
+            var model = new PatronModelCollection()
+            {
+                ListOfPatronModels = listingResult
+            };
+            return View(model);
+        }
+
+    }
+}
---
+++ Libs/Models/Patron/PatronModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Libs.Models.Patron
+{
+    public class PatronModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
